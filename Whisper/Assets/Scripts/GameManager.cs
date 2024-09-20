f9265ac12a4e21f5f402dfa951dc6f8bf845df76
Hodawn <happygy925@naver.com>
2024-09-20 16:05:52 +0900

    Feat : "씬 전환 및 게임 종료 코드 작성"

---
+++ Whisper/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+   public static GameManager Instance;
+
+    private void Awake()
+    {
+        // GameManager 인스턴스가 하나만 존재하도록 보장
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬이 바뀌어도 GameManager가 파괴되지 않도록 설정
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
