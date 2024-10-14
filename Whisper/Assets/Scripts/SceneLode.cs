051e259c5db4f5c657806f10e77e5a927b864c4c
changsu705 <2021137031@g.shingu.ac.kr>
2024-10-14 23:23:57 +0900

    feat : GameManager 버그 해결 player's Room 정장작동 시킴 SceneLode 스크립트 생성
    게임메니저 싱글톤 방식으로 설정 게임메니저에 있는 LoadScene 메소드 SceneLode에서 불러와 작동가능.

---
+++ Whisper/Assets/Scripts/SceneLode.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneLode : MonoBehaviour
+{
+    private Renderer objectRenderer;
+    public Text interactionText; // 문구를 표시할 UI 텍스트
+    private bool isHovering = false; // 현재 호버 상태인지 여부
+
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+
+        // interactionText가 에디터에서 제대로 연결되지 않았을 경우 대비
+        if (interactionText != null)
+        {
+            interactionText.gameObject.SetActive(false); // 처음에는 문구 비활성화
+        }
+        else
+        {
+            Debug.LogError("InteractionText UI가 할당되지 않았습니다.");
+        }
+    }
+
+    // 플레이어가 트리거 콜라이더에 들어왔을 때 호출됨
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // "Player" 태그가 있는 오브젝트와 충돌할 경우
+        {
+            Hover(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (isHovering && Input.GetKeyDown(KeyCode.E))
+        {
+            GameManager.Instance.LoadScene("SceneName");
+            Debug.Log("어쩄든 작동은 됨");
+        }
+    }
+    // 플레이어가 트리거 콜라이더에서 나갔을 때 호출됨
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Hover(false);
+        }
+    }
+
+    // 호버 상태를 켜거나 끄는 함수
+    void Hover(bool isHovering)
+    {
+        this.isHovering = isHovering;
+
+        if (isHovering)
+        {
+            objectRenderer.material.color = Color.green; // 호버 상태일 때 색상 변경
+        }
+        else
+        {
+            objectRenderer.material.color = Color.white; // 호버 상태 아닐 때 기본 색상
+        }
+    }
+}
